d758039d8437903d60ce7af39d867d749c550a26
riquemath <fredabisai@gmail.com>
2020-12-11 07:52:21 +0300

    Add constants in constants helper

--- helpers/constants.helper.js
+++ helpers/constants.helper.js
@@ -1,6 +1,38 @@
+const programTypes = {
+    caregiver: 'CR',
+    ovc: 'OVC',
+    dreams: 'DREAMS'
+}
+const programs = [
+    {
+        id: 'BNsDaCclOiu',
+        childProgram: {
+            id: 'em38qztTI8s',
+            type: programTypes.ovc
+        },
+        type: programTypes.caregiver
+       
+    },
+    {
+        id: 'em38qztTI8s',
+        type: programTypes.ovc,
+        isChild: true
+    },
+    {
+        id: 'hOEIHJDrrvz',
+        type: programTypes.dreams
+    }
+    
+]
+
 const constants = {
     orgUnitLevelThree: 3,
+    programs: programs,
+    programTypes: programTypes,
+    primaryUICMetadataId: 'fTSAY18LgCF',
+    secondaryUICMetadataId: 'eIU7KMx4Tu3'
 }
+
 module.exports = {
     constants
 }
