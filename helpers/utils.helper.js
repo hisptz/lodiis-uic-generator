0ce1154861d40c1b9696b8d2515bf3ac5708daac
riquemath <fredabisai@gmail.com>
2020-12-21 16:27:29 +0300

    Change from Math.round to Math.ceil in getting number of pages

--- helpers/utils.helper.js
+++ helpers/utils.helper.js
@@ -7,8 +7,8 @@ function getDataPaginationFilters(paginationData, pageSize = 50) {
   const pager =
     paginationData && paginationData.pager ? paginationData.pager : {};
   const total =
-    pager && pager.total && pager.total >= pageSize ? pager.total : pageSize;
-  for (let page = 1; page <= Math.round(total / pageSize); page++) {
+    pager &&  pager.total >= pageSize ? pager.total : pageSize;
+  for (let page = 1; page <= Math.ceil(total / pageSize); page++) {
     paginationFilter.push(`totalPages=true&pageSize=${pageSize}&page=${page}`);
   }
   return paginationFilter;
