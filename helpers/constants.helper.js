cfdf0c2e7571365a13861052061c28e4302b403e
riquemath <fredabisai@gmail.com>
2021-01-04 12:37:47 +0300

    get summary columns

--- helpers/constants.helper.js
+++ helpers/constants.helper.js
@@ -1,46 +1,83 @@
 const programTypes = {
-    caregiver: 'CG',
-    ovc: 'OVC',
-    dreams: 'DRM'
-}
+  caregiver: 'CG',
+  ovc: 'OVC',
+  dreams: 'DRM',
+};
 const programs = [
-    {
-        id: 'BNsDaCclOiu',
-        childProgram: {
-            id: 'em38qztTI8s',
-            type: programTypes.ovc
-        },
-        type: programTypes.caregiver
-       
+  {
+    id: 'BNsDaCclOiu',
+    childProgram: {
+      id: 'em38qztTI8s',
+      type: programTypes.ovc,
     },
-    {
-        id: 'em38qztTI8s',
-        type: programTypes.ovc,
-        isChild: true
-    },
-    {
-        id: 'hOEIHJDrrvz',
-        type: programTypes.dreams
-    }
-    
-]
+    type: programTypes.caregiver,
+  },
+  {
+    id: 'em38qztTI8s',
+    type: programTypes.ovc,
+    isChild: true,
+  },
+  {
+    id: 'hOEIHJDrrvz',
+    type: programTypes.dreams,
+  },
+];
 const metadata = {
-    firstname: 'WTZ7GLTrE8Q',
-    surname: 'rSP9c21JsfC'
-}
+  firstname: 'WTZ7GLTrE8Q',
+  surname: 'rSP9c21JsfC',
+  primaryUIC: 'fTSAY18LgCF',
+  secondaryUIC: 'eIU7KMx4Tu3',
+};
+const requestResponse = {
+  summary: {
+    columns: [
+
+         {
+          isAttribute: true,
+          name: 'firstname'
+        },
+        {
+          isAttribute: true,
+          name: 'surname'
+        },
+         {
+          isAttribute: true,
+          name: 'primaryUIC'
+        },
+       {
+          isAttribute: true,
+          name: 'secondaryUIC'
+        },
+       {
+          isAttribute: false,
+          name: 'trackedEntityInstance'
+        },
+       {
+          isAttribute: false,
+          name: 'orgUnit'
+        },
+      {
+          isAttribute: false,
+        name: 'status'
+        },
+
+    ],
+  },
+};
 
 const constants = {
-    orgUnitLevelThree: 3,
-    programs: programs,
-    programTypes: programTypes,
-    primaryUICMetadataId: 'fTSAY18LgCF',
-    secondaryUICMetadataId: 'eIU7KMx4Tu3',
-    metadata: metadata,
-    // primaryUICMetadataId: 'Lo44pBpt230',
-    // secondaryUICMetadataId: 'I5gM3wN4Vsw',
-    ageMetadataId: 'ls9hlz2tyol'
-}
+  orgUnitLevelThree: 3,
+  programs: programs,
+  programTypes: programTypes,
+  primaryUICMetadataId: 'fTSAY18LgCF',
+  secondaryUICMetadataId: 'eIU7KMx4Tu3',
+  metadata: metadata,
+  requestResponse,
+  // primaryUICMetadataId: 'Lo44pBpt230',
+  // secondaryUICMetadataId: 'I5gM3wN4Vsw',
+  ageMetadataId: 'ls9hlz2tyol',
+};
 
 module.exports = {
-    constants
-}
+  constants,
+};
