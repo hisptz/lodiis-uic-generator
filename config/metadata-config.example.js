553cb4f20f894b24f5cca8af4106fc7ecfe3b7ec
megamind <echingalo@gmail.com>
2023-07-10 13:11:57 +0300

    refactor: improved the example metadata config

--- config/metadata-config.example.js
+++ config/metadata-config.example.js
@@ -1,34 +1,47 @@
-const constants = require('../helpers/constants.helper');
-const programTypes = constants.programTypes;
+const programTypes = {
+  caregiver: "CG",
+  ovc: "OVC",
+  dreams: "DRM",
+  bursary: "BUR",
+  lbse: "LB",
+};
+
 const metadata = {
-    firstname: 'firstname_metadata_id',
-    surname: 'surname_metadata_id',
-    primaryUIC: 'primaryUIC_metadata_id',
-    secondaryUIC: 'secondaryUIC_metadata_id',
-      age: 'age_metadata_id',
-  };
+  firstname: "<firstname_metadata_id>",
+  surname: "<surname_metadata_id>",
+  primaryUIC: "<primaryUIC_metadata_id>",
+  secondaryUIC: "<secondaryUIC_metadata_id>",
+  age: "<age_metadata_id>",
+};
 
-  const programs = [
-    {
-      id: 'program_id', // Example: Caregiver
-      childProgram: {
-        id: 'id_of_program_with_child_with_this_program', //Example: ovc
-        type: 'type_of_child_program',
-      },
-      type: 'type_of_program',
+const programs = [
+  {
+    id: "<program-id>",
+    type: "<program-type-as-referred-above>", // programTypes.bursary,
+    isChild: "<true_if_it_is_child_and_false_if_not>",
+  },
+  {
+    id: "<program-id>",
+    childProgram: {
+      id: "<id_of_program_with_child_with_this_program>", //Example: ovc
+      type: "<type_of_child_program>", // programTypes.ovc,
     },
-    {
-      id: 'program_id',
-      type: 'type_of_program',
-      isChild: 'true_if_it_is_child_and_false_if_not',
-    },
-    {
-      id: 'program_id',
-      type: 'type_of_program',
-    },
-  ];
+    type: "<type_of_program>", // programTypes.caregiver,
+  },
+  {
+    id: "<program-id>",
+    type: "<type_of_program>", // programTypes.ovc,
+    isChild: "<true_if_it_is_child_and_false_if_not>",
+  },
+];
 
-  module.exports = {
-    metadata,
-    programs
-}
+module.exports = {
+  metadata,
+  programs,
+  programTypes,
+};
+
+module.exports = {
+  metadata,
+  programs,
+};
